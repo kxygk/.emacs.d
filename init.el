a41fbec1c2697c04922c3e4e47fa52cac768546c
George Kontsevich <geokon@qq.com>
2019-03-31 17:28:25 +0800

    Added: Saving backups to a central folder
    Instead of littering every directory with files edited by Emacs

--- init.el
+++ init.el
@@ -63,6 +63,14 @@
   (moe-light)
   (set-face-attribute 'default nil :background "#ffffff" :foreground "#5f5f5f"))
 
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves/"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2)
+
 (use-package magit
   :ensure t)
 
